a3808cb318642584a320a43a431d2fa984f491f0
leonhatches <jhatches@unsa.edu.pe>
2025-11-09 22:28:41 -0500

    Se comenta las acciones que se deben realizar en la simulacion_1d

---
+++ src/simulacion_1d/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+// Incluir
+using namespace std;
+
+// Definir variables globales
+
+
+// Configuración inicial
+
+
+// Cabecera
+
+
+// Moviviento real del robot
+
+
+// Sensor de profundidad
+
+
+// Incializar partículas
+
+
+// Función de verosimilitud
+
+
+// Normalización (Filtro de partículas)
+
+
+// Estimación de posición
+
+
+// Main
+
